6f184c07ee4699b19b8289cdf44963c361e49566
Nathan <nathanrmsmith@gmail.com>
2023-08-09 18:29:11 -0400

    Add function to request script

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,24 @@
 const request = require('request');
 
-const userInput = process.argv.slice(2);
-const breedType = userInput[0].toLowerCase();
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `http://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-request(`http://api.thecatapi.com/v1/breeds/search?q=${breedType}`, function(error, response, body) {
-  if (error) {
-    console.log("Oh No! The request failed: ", error && response.statusCode);
-    return;
-  }
-  
-  const data = JSON.parse(body);
+  request(url, function(error, response, body) {
+    
+    if (error) {
+      callback(error, null);
+      return;
+    }
 
-  // Checks if data is returned from the request
-  if (data.length === 0) {
-    console.log(`The request went through: Response Code ${response.statusCode}. YAY! \nHowever, there is no data as you entered an incorrect breedtype: ${breedType}`);
-    return;
-  }
-  
-  console.log(data[0].description);
-  return data[0].description;
-});
+    const data = JSON.parse(body);
+
+    if (data.length === 0) {
+      callback("Breed Not Found", null);
+      return;
+    }
+
+    callback(null, data[0].description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
